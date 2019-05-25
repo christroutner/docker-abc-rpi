e43a3c097cdfc2af3158284e670daa923388e821
Chris Troutner <chris.troutner@gmail.com>
2019-05-25 16:41:04 +0000

    Configuring for testnet

--- run-image.sh
+++ run-image.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 docker container run --name bitcoind -d \
 -p 8332:8332 -p 8333:8333 -p 18332:18332 -p 18333:18333 \
--v /mnt/usb:/data \
+-v /media/pi/137ac8d6-c99f-4206-a771-89eeff117a13/bch-abc-testnet-blockchain:/data \
 bitcoind
